080b887939533f8aa90acb2979e7f6a47dece500
FeiYe <316606233@qq.com>
2023-02-25 18:26:49 +0800

    new: wrap

--- src/index.ts
+++ src/index.ts
@@ -34,30 +34,39 @@ export type PropertyDecoratorCallback<T> = (type: Object, target: Object, proper
  * deco.getTypes(Target.prototype);
  * ```
  */
-export function makePropertyDecorator<T = any>(cb?: PropertyDecoratorCallback<T>) {
+export function makePropertyDecorator<T = any>() {
   const TYPES = Symbol('makePropertyDecorator#types');
 
   /**
-   * 属性装饰器
+   * 包裹器，用于装饰器自定义处理
    * 
-   * @param target 目标对象
-   * @param propertyKey 目标对象中的属性名
-   * @param type 属性类型，在不使用 Decorators 特性时需要指定
+   * @param cb 回调处理函数
+   * @returns 装饰器
    */
-  function decorate(target: Object, propertyKey: string | symbol, type?: Object) {
-    if (!type) {
-      type = Reflect.getOwnMetadata('design:type', target, propertyKey);
+  function wrap(cb?: PropertyDecoratorCallback<T>) {
+    /**
+     * 属性装饰器
+     *
+     * @param target 目标对象
+     * @param propertyKey 目标对象中的属性名
+     * @param type 属性类型，在不使用 Decorators 特性时需要指定
+     */
+    function decorate(target: Object, propertyKey: string | symbol, type?: Object) {
+      if (!type) {
+        type = Reflect.getOwnMetadata('design:type', target, propertyKey);
+      }
+      if (!type) {
+        throw new Error(`Missing propertie type: ${targetName(target)}::${String(propertyKey)}`);
+      }
+      // 将父类的装饰属性描述复制到子类中，不能修改父类的装饰属性
+      const types = Object.assign(
+        {},
+        getTypes(target),
+        { [propertyKey]: cb ? cb(type, target, propertyKey) : type },
+      );
+      Reflect.defineMetadata(TYPES, types, target);
     }
-    if (!type) {
-      throw new Error(`Missing propertie type: ${targetName(target)}::${String(propertyKey)}`);
-    }
-    // 将父类的装饰属性描述复制到子类中，不能修改父类的装饰属性
-    const types = Object.assign(
-      {},
-      getTypes(target),
-      { [propertyKey]: cb ? cb(type, target, propertyKey) : type },
-    );
-    Reflect.defineMetadata(TYPES, types, target);
+    return decorate;
   }
 
   /**
@@ -69,7 +78,8 @@ export function makePropertyDecorator<T = any>(cb?: PropertyDecoratorCallback<T>
   }
 
   return {
-    decorate,
+    wrap,
+    decorate: wrap(),
     getTypes,
   };
 }
@@ -114,35 +124,45 @@ export type MethodDecoratorCallback<T> = (
  * 
  * @param cb 装饰处理回调
  */
-export function makeMethodDecorator<T = MethodDescriptor>(cb?: MethodDecoratorCallback<T>) {
+export function makeMethodDecorator<T = MethodDescriptor>() {
   const METHODS = Symbol('makeMethodDecorator#methods');
 
   /**
-   * 方法装饰器
+   * 包裹器，用于装饰器自定义处理
    * 
-   * @param target 目标对象
-   * @param propertyKey 目标对象中方法名
-   * @param descriptor_or_paramtypes 目标对象中方法描述 | 或不使用 experimentalDecorators 特性时需要被注入方法参数列表
+   * @param cb 回调处理函数
+   * @returns 装饰器
    */
-  function decorate(target: Object, propertyKey: string | symbol, descriptor_or_paramtypes?: PropertyDescriptor | any[]) {
-    let handle: MethodHandle;
-    let params: any[];
-    // 兼容 js 调用
-    if (!descriptor_or_paramtypes || Array.isArray(descriptor_or_paramtypes)) {
-      handle = (<any>target)[propertyKey];
-      params = descriptor_or_paramtypes || [];
-    // ts 调用
-    } else {
-      handle = descriptor_or_paramtypes.value;
-      params = Reflect.getOwnMetadata('design:paramtypes', target, propertyKey) || [];
+  function wrap(cb?: MethodDecoratorCallback<T>) {
+    /**
+     * 方法装饰器
+     * 
+     * @param target 目标对象
+     * @param propertyKey 目标对象中方法名
+     * @param descriptor_or_paramtypes 目标对象中方法描述 | 或不使用 experimentalDecorators 特性时需要被注入方法参数列表
+     */
+    function decorate(target: Object, propertyKey: string | symbol, descriptor_or_paramtypes?: PropertyDescriptor | any[]) {
+      let handle: MethodHandle;
+      let params: any[];
+      // 兼容 js 调用
+      if (!descriptor_or_paramtypes || Array.isArray(descriptor_or_paramtypes)) {
+        handle = (<any>target)[propertyKey];
+        params = descriptor_or_paramtypes || [];
+      // ts 调用
+      } else {
+        handle = descriptor_or_paramtypes.value;
+        params = Reflect.getOwnMetadata('design:paramtypes', target, propertyKey) || [];
+      }
+
+      const methodDescriptor = { handle, params };
+      const methods = [
+        ...getMethods(target),
+        cb ? cb(methodDescriptor, target, propertyKey) : methodDescriptor,
+      ];
+      Reflect.defineMetadata(METHODS, methods, target);
     }
 
-    const methodDescriptor = { handle, params };
-    const methods = [
-      ...getMethods(target),
-      cb ? cb(methodDescriptor, target, propertyKey) : methodDescriptor,
-    ];
-    Reflect.defineMetadata(METHODS, methods, target);
+    return decorate;
   }
 
   /**
@@ -154,7 +174,8 @@ export function makeMethodDecorator<T = MethodDescriptor>(cb?: MethodDecoratorCa
   }
 
   return {
-    decorate,
+    wrap,
+    decorate: wrap(),
     getMethods,
   };
 }
@@ -187,15 +208,25 @@ export type ClassDecoratorCallback<T> = (target: any) => T;
  * 
  * @param value 装饰完成后赋予的值，用于 `decorator.getValue(target)` 取出
  */
-export function makeClassDecorator<T = true>(cb?: ClassDecoratorCallback<T>) {
+export function makeClassDecorator<T = true>() {
   const VALUE = Symbol('makeClassDecorator#value');
 
   /**
-   * 类装饰器
-   * @param target 目标类
+   * 包裹器，用于装饰器自定义处理
+   * 
+   * @param cb 回调处理函数
+   * @returns 装饰器
    */
-  function decorate(target: Object) {
-    Reflect.defineMetadata(VALUE, cb ? cb(target) : true, target);
+  function wrap(cb?: ClassDecoratorCallback<T>) {
+    /**
+     * 类装饰器
+     * @param target 目标类
+     */
+    function decorate(target: Object) {
+      Reflect.defineMetadata(VALUE, cb ? cb(target) : true, target);
+    }
+
+    return decorate;
   }
 
   /**
@@ -207,7 +238,8 @@ export function makeClassDecorator<T = true>(cb?: ClassDecoratorCallback<T>) {
   }
 
   return {
-    decorate,
+    wrap,
+    decorate: wrap(),
     getValue,
   };
 }
